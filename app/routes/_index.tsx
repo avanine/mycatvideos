c62b83ce2fa337c63e646cbcbfb5ffcccd7bc9a5
Marike <59769927+avanine@users.noreply.github.com>
2024-03-27 19:47:53 +0200

    Initial Remix setup

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "My Cat Videos" },
+    { name: "description", content: "Cat videos!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
